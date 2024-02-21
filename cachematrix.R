a747135c633ef955d7e8c6314d3ffea361411745
ChefSarevok <160761143+ChefSarevok@users.noreply.github.com>
2024-02-21 08:53:04 -0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains the two functions to meet the requirements for the 
+## Introduction to R course, Project 2, with overall goals of calculating an
+## inverse matrix and caching the results.
 
-## Write a short comment describing this function
+## This function creates a cached matrix "x", within the function "y" and 
+## inverse "i".
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+    i <- NULL # Creation of inverse variable "i"
+    set <- function(y) {
+      x <<- y # Binding the open matrix argument to variable "y"
+      i <<- NULL
+    }
+    
+    get <- function() x #Caching the function "y", the matrix data, to 
+    #lone function and applying inverse base function
+    setinverse <- function(inverse) i <<- inverse
+    getinverse <- function() i
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## Runs the solve function on the matrix cached in "getinverse" applying 
+## the inverse calculations. Returns inverse "x" = "i".
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+      i <- x$getinverse()
+    if(!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+      
+    data <- x$get()
+    i <- solve(data)
+    x$setinverse(i)
+    i
+  }
+
